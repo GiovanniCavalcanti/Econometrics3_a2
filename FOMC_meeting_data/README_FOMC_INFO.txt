f5c6c9e671f504aaedfcfaeb8b9f2f3205dbd9ad
GiovanniCavalcanti <giovannic1@al.insper.edu.br>
2024-02-22 10:08:37 -0300

    FOMC data extraction

---
+++ FOMC_meeting_data/README_FOMC_INFO.txt
@@ -0,0 +1,9 @@
+Data collected from 
+https://www.federalreserve.gov/monetarypolicy/materials/
+Specifically, the following reports were downloaded and read:
+- Greenbooks;
+- Minutes (1993-Present);
+- Policy Statements;
+- Minutes of actions (1967-1992);
+- Records of Action (1936-1992);
+
---
+++ FOMC_meeting_data/script_extract_fomc_data.R
@@ -0,0 +1,23 @@
+### Data for meeting 01/14/1969 (MDY)
+## Url tupe for bluebook
+# https://www.federalreserve.gov/monetarypolicy/files/FOMC19690114bluebook19690110.pdf
+## Url type for greenbook 
+# https://www.federalreserve.gov/monetarypolicy/files/FOMC19690114greenbook19690108.pdf
+## Url type for greenbook suplement
+# https://www.federalreserve.gov/monetarypolicy/files/FOMC19690114gbsup19690110.pdf
+## Url type for Minutes of Action 
+# https://www.federalreserve.gov/monetarypolicy/files/fomcmoa19690114.pdf
+## Url type for Record of Policy Actions
+# https://www.federalreserve.gov/monetarypolicy/files/fomcropa19690114.pdf
+
+### Data for meeting 12/17/1996 (MDY)
+## Url tupe for bluebook
+# https://www.federalreserve.gov/monetarypolicy/files/fomc19961217bluebook19961213.pdf
+## Url type for greenbook part1 
+# https://www.federalreserve.gov/monetarypolicy/files/fomc19961217gbpt119961212.pdf
+## Url type for greenbook part2
+# https://www.federalreserve.gov/monetarypolicy/files/fomc19961217gbpt219961212.pdf
+## Url type for greenbook suplement
+# https://www.federalreserve.gov/monetarypolicy/files/fomc19961217gbsup19961213.pdf
+## HTML for Minutes
+# https://www.federalreserve.gov/fomc/minutes/19961217.htm
